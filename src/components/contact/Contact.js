10b8c1738f5efa87041c797887a9660387084b37
Garima Jyoti <jyotigarima2000@gmail.com>
2023-09-03 20:52:55 -0400

    Added contact component and its styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,17 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact_container'>
+        <div className='contact_info'>
+          <small>Want to know more, contact me</small>
+          <h3>jyotigarima2000@gmail.com</h3>
+        </div>
+      </div>
+    </section>
   )
 }
 
