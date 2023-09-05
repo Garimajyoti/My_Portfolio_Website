08d1d78cdd23b6f0f3305bdf86f39365ae58f894
Garima Jyoti <jyotigarima2000@gmail.com>
2023-09-05 16:13:33 -0400

    changes in nav component

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,13 +4,18 @@ import './nav.css'
 const Nav = () => {
   return (
     <nav>
-      <div id='nav-links'>
-      <a href="#">Home</a>
-      <a href="#about">About</a>
-      <a href="#experience">Experience</a>
-      <a href="#services">Services</a>
-      <a href="#testimonials">Testimonials</a>
-      <a href="#contact">Contact</a>
+      <div className='container nav_container'>
+        <div className="logo">
+          <h2>PORTFOLIO</h2>
+        </div>
+        <ul className="nav_links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
       </div>
     </nav>
   )
